a5266e34215f3ca9e3e708488d536f28ae8e79c2
ogbobe <cosmas.akanna@gmail.com>
2025-06-18 07:28:17 +0100

    mine

---
+++ project_descrp.py
@@ -0,0 +1,22 @@
+'''Write a python program to manage student registration.
+-   to Add a student record to a List
+         - name (firstname, lastname)
+         - address
+         - phone nos (guardian and/or personal) - no duplicate
+                - guardian (1 only, compulsory)
+                - personal (max 2, optional)
+         - emails (max 2) - no duplicate
+         - subjects: 4 of (English*, Maths*, Physics, Chemistry, Commerce, Economics, Literature), Eng n Math compulsory.
+
+-   search for a student,
+-     - print the details
+-     - edit
+-            - firstname
+-            - address
+-            - phone nos (add, remove, change)
+-            - emails (add, remove, change)
+- for emails, First email is the main.. if added, cannot be removed or changed
+- You cannot save a student record without a guardian phone no or main email.
+
+
+Using variables, string literals, string methods, tuple, range, List, dictionary, Loop (for, while), Conditional Statement (if else, match case), function'''
